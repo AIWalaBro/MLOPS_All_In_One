19161e2a246093d3cb3e263ac869ee21d6ffac78
AIWalaBro <aiwalabro@gmail.com>
2024-06-29 18:15:10 +0530

    folder structure updated

--- template.py
+++ template.py
@@ -1,4 +1,29 @@
 import os
+from pathlib import Path
+
+package_name = "mongodb-connect"
+
+list_of_files = [
+".github/workflows/ci.yaml",
+"src/_init _. py",
+f"src/{package_name}/_init _. py",
+f"src/{package_name}/mongo_crud.py",
+"tests/_init _. py",
+"tests/unit/_init _. py",
+"tests/unit/unit.py",
+"tests/integration/_init _. py",
+"init_setup.sh",
+"requirements.txt",
+"requirements_dev.txt",
+"setup.py",
+"setup.cfg",
+"pyproject.toml",
+"tox.ini",
+"experiments/experiments.ipynb",
+
+]
+
+import os
 import sys
 from pathlib import Path
 
