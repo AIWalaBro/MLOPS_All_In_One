1e63ed2b3ad9ec49ea268be9b7ffad2c60652645
AIWalaBro <aiwalabro@gmail.com>
2024-07-09 09:09:12 +0530

    testing file created for testing

---
+++ test.py
@@ -0,0 +1,8 @@
+from src.logger.logger import logging
+# from src.exception.exception import CustomException
+
+# Create a logger
+
+# logging.info("Log file testing")
+print(logging.info("Log file testing second time"))
+
