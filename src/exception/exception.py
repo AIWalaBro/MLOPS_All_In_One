3485f39bf13f3cb5ebbffb675608bd61cc2973f3
AIWalaBro <aiwalabro@gmail.com>
2024-07-09 09:46:48 +0530

    exception code updated

---
+++ src/exception/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+class customexception(Exception):
+    def __init__(self, error_message, error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        print(exc_tb)
+        
+        self.line_no = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        
+        
+    def __str__(self) -> str:
+        return f"Error occured in python script name {self.file_name} at line number {self.line_no} and error message is {self.error_message}"
+    
+if __name__ == "__main__":
+    try:
+       a = 1/0
+    except Exception as e:
+        raise customexception(e,sys)
